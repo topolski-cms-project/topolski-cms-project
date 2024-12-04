31b8b9bd9c1c95edec7c4b246c7cddeef816b3ee
Szczepan Topolski <szczepanPL678@gmail.com>
2024-12-04 20:46:06 +0100

    modify migration and configuration

---
+++ backend/src/main/resources/db/migration/local/V2__add_values_to_product_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (name, price, image_url) VALUES
+    ('Product 1', 19.99, 'https://example.com/images/product1.jpg'),
+    ('Product 2', 29.99, 'https://example.com/images/product2.jpg'),
+    ('Product 3', 39.99, 'https://example.com/images/product3.jpg'),
+    ('Product 4', 49.99, 'https://example.com/images/product4.jpg');
