5a726e7cff3958434f0cdc7797e76ef09d9c5a1e
Szczepan Topolski <szczepanPL678@gmail.com>
2024-12-05 19:23:56 +0100

    Add endpoint for product details

--- backend/src/main/java/com/topolski/backend/model/product/entity/TechnicalDetails.java
+++ backend/src/main/java/com/topolski/backend/model/product/entity/TechnicalDetails.java
@@ -1,11 +1,15 @@
 package com.topolski.backend.model.product.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class TechnicalDetails {
 
